fd60667858cfd76fbe5c8490cd914f0da4a2a469
Roger <roger.neil.davenport@gmail.com>
2018-03-22 10:38:01 -0700

    game promotion save not working

--- model/game-model.js
+++ model/game-model.js
@@ -7,9 +7,9 @@ const Game = mongoose.Schema({
   'startTime': {type: Date},
   'endTime': {type: Date},
   'division': {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'division'},
-  'teamA': {type: mongoose.Schema.Types.ObjectId, ref: 'team'},
+  'teamA': {type: mongoose.Schema.Types.ObjectId, ref: 'team', default: null},
   'teamAResult': {type: Number},
-  'teamB': {type: mongoose.Schema.Types.ObjectId, ref: 'team'},
+  'teamB': {type: mongoose.Schema.Types.ObjectId, ref: 'team', defalut: null},
   'teamBResult': {type: Number},
   'complete': {type: Boolean},
 
